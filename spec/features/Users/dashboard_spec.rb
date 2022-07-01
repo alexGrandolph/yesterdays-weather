6d40af95969ecb5d3287e369a361459b7e143025
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-01 11:44:59 -0600

    test; add test for user dashboard having link to add location, link takes to /locations/new

--- spec/features/Users/dashboard_spec.rb
+++ spec/features/Users/dashboard_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User Dashboard' do
 
-  it 'filling in sign up form correctly redirects to users dashboard' do
+  it 'displays the users username' do
     visit '/register'
 
     fill_in 'Username:', with: 'Skeeter8'
@@ -13,6 +13,20 @@ RSpec.describe 'User Dashboard' do
     click_on 'Submit'
     
     expect(page).to have_content('Welcome, Skeeter8')
-
   end
+
+  it 'has a link to add a location' do
+    visit '/register'
+
+    fill_in 'Username:', with: 'Skeeter8'
+    fill_in 'Email:', with: 'skeeter@corgi.com'
+    fill_in 'Phone Number:', with: '3033033033'
+    fill_in 'Password:', with: 'test'
+    fill_in 'Password Confirmation:', with: 'test'
+    click_on 'Submit'
+    
+    expect(page).to have_link('Add Location')
+  end
+
+
 end
