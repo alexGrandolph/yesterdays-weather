35aa6cd47034ec544dff2f9153b650c0725c2a91
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-01 11:35:48 -0600

    feat: add /register route to users#new and add new users/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   root "welcome#index"
+
+  get '/register', to: "users#new"
 end
--- spec/features/Users/new_spec.rb
+++ spec/features/Users/new_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'New User Page' do
   it 'clicking link to Sign Up brings user to new user page' do
     visit '/'
     click_on 'Sign Up'
+    save_and_open_page
     expect(current_path).to eq('/register')
   end
 end
