d6c401c5ffed8816a5f68331a6ec30462c60954e
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-01 11:56:18 -0600

    test: add test for adding a new location via form for current_user

---
+++ spec/features/Users/locations/new_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'User Dashboard' do
+  before do
+    visit '/register'
+
+    fill_in 'Username:', with: 'Skeeter8'
+    fill_in 'Email:', with: 'skeeter@corgi.com'
+    fill_in 'Phone Number:', with: '3033033033'
+    fill_in 'Password:', with: 'test'
+    fill_in 'Password Confirmation:', with: 'test'
+    click_on 'Submit'
+
+  end 
+  it 'displays the users username' do
+    click_link "Add Location"
+    fill_in 'Location:', with: 'Evergreen,CO'
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("Evergreen,CO")
+  end
