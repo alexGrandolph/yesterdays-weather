a7f019377b5d86365b723aa1e59ceec7af3e3079
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-01 11:33:05 -0600

    test: add test for link to sign up taking to /register

---
+++ spec/features/Users/new_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'New User Page' do
+  it 'clicking link to Sign Up brings user to new user page' do
+    visit '/'
+    click_on 'Sign Up'
+    expect(current_path).to eq('/register')
+  end
+end
--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Welcome/Homepage' do
   it 'Has a header with links for home, signup, if there is no current_user' do
     visit "/"
-    save_and_open_page
+
     expect(page).to have_link("Home")
     expect(page).to have_link("Sign Up")
-
   end
 end
