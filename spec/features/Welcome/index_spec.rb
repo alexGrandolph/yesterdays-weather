e925623cd03f950cff141490b1c84bcd9c764f47
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-01 11:29:09 -0600

    feat: add route route / and welcome controller #index and view

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Welcome/Homepage' do
   it 'Has a header with links for home, signup, if there is no current_user' do
     visit "/"
-
+    save_and_open_page
     expect(page).to have_link("Home")
     expect(page).to have_link("Sign In")
 
