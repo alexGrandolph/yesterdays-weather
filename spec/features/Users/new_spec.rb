fe8ec1faae5b009dc5c6f6625eb0124093fe5a39
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-07-01 11:38:17 -0600

    test: add test for filling in new user form correctly taking to users dashboard

--- spec/features/Users/new_spec.rb
+++ spec/features/Users/new_spec.rb
@@ -4,7 +4,19 @@ RSpec.describe 'New User Page' do
   it 'clicking link to Sign Up brings user to new user page' do
     visit '/'
     click_on 'Sign Up'
-    save_and_open_page
     expect(current_path).to eq('/register')
+  end
+
+  it 'filling in sign up form correctly redirects to users dashboard' do
+    visit '/register'
+
+    fill_in 'Username:', with: 'Skeeter8'
+    fill_in 'Email:', with: 'skeeter@corgi.com'
+    fill_in 'Phone Number:', with: '3033033033'
+    fill_in 'Password:', with: 'test'
+    fill_in 'Password Confirmation:', with: 'test'
+
+    expect(current_path).to eq('/dashboard')
+
   end
 end
